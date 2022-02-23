547cee1bde4888e68d9824bc3cb023528f2086f0
Erutaner <renaissancezj@gmail.com>
2022-02-23 18:00:26 +0800

    my_strcmp

--- 源.c
+++ 源.c
@@ -1,21 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<assert.h>
-char* my_strcat(char* dst, const char* src)
+int my_strcmp(const char* c1, const char* c2)
 {
-	assert(dst && src);
-	char* cp = dst;
-	while (*cp)
-		cp++;
-	while (*cp++ = *src++);
-	return (dst);
+	assert(c1 && c2);
+	while (*c1 == *c2)
+	{
+		if (*c1 == 0)
+			return 0;
+		c1++;
+		c2++;
+	}
+	return (*c1 - *c2);
+
 }
 int main(void)
 {
-	char arr1[] = "hello \0xxxxxx";
-	char arr2[] = "world";
-	printf("%s", my_strcat(arr1, arr2));
-
+	char* p1 = "abcze";
+	char* p2 = "abcze";
+	
+	if (my_strcmp(p1, p2) > 0)
+		printf("p1>p2");
+	else if (my_strcmp(p1,p2) == 0)
+		printf("p1=p2");
+	else if (my_strcmp(p1,p2) < 0)
+		printf("p1<p2");
 
 	return 0;
 }
