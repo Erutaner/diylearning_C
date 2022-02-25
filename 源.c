24f7851f01ad99ad1756538002de7ce408d15765
Erutaner <renaissancezj@gmail.com>
2022-02-25 23:38:16 +0800

    my_memcpy

--- 源.c
+++ 源.c
@@ -1,33 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<assert.h>
-char* my_strstr(const char* str1, const char* str2)
+#include<ctype.h>
+void* my_memcpy(void* dest, const void* source, size_t num)
 {
-	assert(str1 && str2);
-	char* cp = (char*)str1;
-	char* s1, *s2;//注意s2前面也要加星号
-	if (!*str2)
-		return (char*)str1;
-	while (*cp)
-	{
-		s1 = cp;
-		s2 =(char*) str2;
-		while (*s1 && *s2 && !(*s1 - *s2))
-			s1++, s2++;
-		if (!*s2)
-			return cp;
-		cp++;
-	}
-	return NULL;
-
-
+	assert(dest && source);
+	void* ret = dest;
+	while (num--)
+		*((char*)dest)++ = *((char*)source)++;
+	return (ret);
 }
+typedef struct S
+{
+	char name[20];
+	int age;
+}S;
 int main(void)
 {
 
-	char* str1 = "123456777";
-	char* str2 = "456";
-	printf("%s", my_strstr(str1, str2));
-
+	int arr[] = { 1,2,3,4 };
+	int arr2[4] = { 0 };
+	my_memcpy(arr2, arr, sizeof(arr));
 	return 0;
 }
